efdb22d16147cf504491bd0acf54448c47fc9566
Ryan Vank <vankerkr@gmatc.matc.edu>
2019-12-12 19:06:19 -0600

    added endpoints

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from "./models/User";
 import auth from './middleware/auth';
+import Post from './models/Post';
 
 //Init express application
 const app = express();
@@ -161,6 +162,119 @@ const returnToken = (user, res) => {
     }
   )
 }
+
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+  '/api/posts',
+  [
+    auth,
+    [
+        check('title', 'Title text is required')
+      .not()
+      .isEmpty(),
+      check('body', 'Body text is required')
+      .not()
+      .isEmpty()
+    ]
+    
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      const { title, body } = req.body;
+      try {
+        //Get the user who created the post
+        const user = await User.findById(req.user.id);
+        
+        //Create a new post
+        const post = new Post({
+            user: user.id,
+            title: title,
+            body:body
+        });
+
+        // Save to the db and return
+        await post.save();
+
+        res.json(post);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+      }
+    }
+  }
+);
+
+/**
+ * @route GET api/posts
+ * @desc Get Posts
+ */
+app.get('/api/posts', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+
+/**
+ * @route GET api/posts/:id
+ * @desc Get Post
+ */
+app.get('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if(!post){
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+
+app.delete('/api/posts/:id', auth, async (req,res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //make sure the post was found
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found'});
+    }
+
+    //Make sure the request user created the post
+    if( post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized'});
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed'});
+  } catch(error) {
+    console.error(error);
+    res.status(500).send('Server error')
+  }
+})
+
+
 
 //Connection Listener
 const port = 5000;
